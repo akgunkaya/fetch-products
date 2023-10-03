e1417bd5ad87bff757a810ac05db6475c8d08f4b
Akgun Kaya <akgunkaya@Akguns-MacBook-Air.local>
2023-10-03 18:37:13 +0100

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div> 
+      <Head>
+        <title>Fetch Products App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>  
+
+      <main>
+        <h1>TEST</h1>
+      </main>   
+
+
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
